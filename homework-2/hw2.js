65d92e9251672ca8091a73f589c141790b1f57e6
Michael Grachev <mv.grachev@yandex.ru>
2020-06-02 16:13:26 +0300

    Внесение исправлений в заглушку

--- homework-2/hw2.js
+++ homework-2/hw2.js
@@ -1,16 +1,16 @@
-function compare_numbers(x, y){
-   if (a > b)
-      alert("x > y");
-   else if (x < y)
-      alert("x < y");
-   else
-      alert("x = y");
+function compare_numbers(a, b){
+	if (a >= 0 && b >= 0 )
+		alert(b - a);
+	else if (a < 0 && b < 0 )
+		alert(a * b);
+	else if ( (a < 0 && b > 0) || (a > 0 && b < 0 ) )
+		alert(a + b);
 }
 
 function InputOperands () {
 	let a = prompt('Введите значения переменной a:', 0);
 	let b = prompt('Введите значения переменной b:', 0);
-	let re = '/\D//g';
+	let re = '/[^-]\D//g';
 	a = a.replace(re);
 	b = b.replace(re);
 	if ( a == '' || b == '' ) {
@@ -19,7 +19,7 @@ function InputOperands () {
 	
 	let hash = new Map([
   		['a', a],
-  		['a', b]
+  		['b', b]
 	]);
 	return hash;
 }
@@ -27,4 +27,4 @@ function InputOperands () {
 let operands = new Map();
 operands = InputOperands();
 
-alert(operands.get("a"));
+compare_numbers( operands.get("a"), operands.get("b"));
